09f67a50e6cdb382bc83ba1a359af1be74e6b54b
v-liuson <v-liuson@MSDNUWPSERVER1>
2016-12-15 10:23:19 +0800

    Add project files.

---
+++ SplitViewSupport/ChangeText.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SplitViewSupport
+{
+        public class ChangeText
+        {
+            public void ChangeTextNow(myObject myObject)
+            {
+                myObject.MyText = "Banana";
+            }
+        }
+}
---
+++ SplitViewSupport/myObject.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SplitViewSupport
+{
+    public class myObject : INotifyPropertyChanged
+    {
+        private string myText;
+
+        public string MyText
+        {
+            get { return this.myText; }
+            set
+            {
+                this.myText = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void NotifyPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+
+}
